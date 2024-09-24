1f2c862e0a882acead15d855c20f64b5f518daa2
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-09-24 14:31:40 +0600

    Update icon.ts

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,42 +2,42 @@ import type { Favicon } from '@/types/config.ts'
 
 export const defaultFavicons: Favicon[] = [
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'light',
     sizes: '32x32',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'light',
     sizes: '128x128',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'light',
     sizes: '180x180',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'light',
     sizes: '192x192',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'dark',
     sizes: '32x32',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'dark',
     sizes: '128x128',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: 'https://i.ibb.co.com/fkLJs2G/freeguybd-favicon-1.png',
+    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
     theme: 'dark',
     sizes: '192x192',
   },
