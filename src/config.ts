7aa5632cd03d3d6a39675c6f07198f8c76e62275
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-09-27 10:44:44 +0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -19,9 +19,9 @@ export const siteConfig: SiteConfig = {
     src: 'https://i.ibb.co.com/z5KtZxJ/welcome-to-freeguybd.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: 'For Download Movies Visit Here.',              // Credit text to be displayed
-      url: 'https://cinebuzzbd.pages.dev'                // (Optional) URL link to the original artwork or artist's page
+      enable: true,         // Display the credit text of the banner image
+      text: 'For Watch live Sports Visit Here.',              // Credit text to be displayed
+      url: 'https://sportbuzz.netlify.app'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   favicon: [    // Leave this array empty to use the default favicon
