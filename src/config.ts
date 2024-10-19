91dec311919dc344bb556303c64483e0b87fe522
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-19 12:59:42 +0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'CineBuzZBD',
+  title: 'FREEGUYBD',
   subtitle: 'Download movies for nothing',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
