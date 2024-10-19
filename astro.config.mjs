63520a0d6442e3619acf303cc27b9a62b24b02fe
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-10-19 13:00:43 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cinebuzzbd.netlify.app/",
+  site: "https://freeguybd.netlify.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
