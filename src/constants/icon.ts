2c0ad5e186186bcefca9167f8877611ac3b646ac
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2024-10-19 13:17:16 +0600

    Update icon.ts

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,42 +2,42 @@ import type { Favicon } from '@/types/config.ts'
 
 export const defaultFavicons: Favicon[] = [
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'light',
     sizes: '32x32',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'light',
     sizes: '128x128',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'light',
     sizes: '180x180',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'light',
     sizes: '192x192',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'dark',
     sizes: '32x32',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'dark',
     sizes: '128x128',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: 'https://raw.githubusercontent.com/siam3310/si-movies/main/CB.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
     theme: 'dark',
     sizes: '192x192',
   },
