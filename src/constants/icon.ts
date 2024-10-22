6cbce05ce25e1d566511e011dadf2890387da1fa
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2024-10-22 14:16:15 +0600

    Update icon.ts

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,42 +2,42 @@ import type { Favicon } from '@/types/config.ts'
 
 export const defaultFavicons: Favicon[] = [
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'light',
     sizes: '32x32',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'light',
     sizes: '128x128',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'light',
     sizes: '180x180',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'light',
     sizes: '192x192',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'dark',
     sizes: '32x32',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'dark',
     sizes: '128x128',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/freeguybd-favicon.png',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
     theme: 'dark',
     sizes: '192x192',
   },
