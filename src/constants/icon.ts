7e3c327002c5a88d168502bd916b539caee00fd5
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2024-11-12 19:53:05 +0600

    Update icon.ts

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,42 +2,42 @@ import type { Favicon } from '@/types/config.ts'
 
 export const defaultFavicons: Favicon[] = [
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'light',
     sizes: '32x32',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'light',
     sizes: '128x128',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'light',
     sizes: '180x180',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'light',
     sizes: '192x192',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'dark',
     sizes: '32x32',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'dark',
     sizes: '128x128',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/man-person-icon.svg',
+    src: 'https://upload-cloudfalre-r2-assets.omrecipes.dev/cropped_image.png',
     theme: 'dark',
     sizes: '192x192',
   },
